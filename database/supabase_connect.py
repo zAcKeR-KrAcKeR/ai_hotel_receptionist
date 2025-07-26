5df92d5f6fa76572ce165583f47d24dddd8c7b08
Vishnu Jaiswal <30700361+zAcKeR-KrAcKeR@users.noreply.github.com>
2025-07-26 17:42:05 +0530

    Update supabase_connect.py

--- database/supabase_connect.py
+++ database/supabase_connect.py
@@ -1,1 +1,11 @@
-
+import os
+from supabase import create_client
+from dotenv import load_dotenv
+load_dotenv()
+class SupabaseConnection:
+    def __init__(self):
+        self.url = os.getenv("SUPABASE_URL")
+        self.key = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
+        self.client = create_client(self.url, self.key)
+supabase_connection = SupabaseConnection()
+supabase = supabase_connection.client
