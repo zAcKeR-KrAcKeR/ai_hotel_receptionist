fd1c4685986889c0cc1c344adbd4048aeaae014f
Vishnu Jaiswal <30700361+zAcKeR-KrAcKeR@users.noreply.github.com>
2025-07-26 17:43:35 +0530

    Update audio_handler.py

--- utils/audio_handler.py
+++ utils/audio_handler.py
@@ -1,1 +1,17 @@
+import requests, tempfile, os
+class AudioHandler:
+    @staticmethod
+    def download_audio_from_url(audio_url, output_path=None):
+        if not output_path:
+            temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".wav")
+            output_path = temp_file.name
+            temp_file.close()
+        r = requests.get(audio_url)
+        with open(output_path, "wb") as f:
+            f.write(r.content)
+        return output_path
+    @staticmethod
+    def cleanup_temp_file(filepath):
+        if filepath and os.path.exists(filepath):
+            os.unlink(filepath)
 
