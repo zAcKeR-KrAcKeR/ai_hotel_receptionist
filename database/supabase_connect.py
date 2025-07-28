50b602f03cff2e8a1790a7a310b140c074435acc
Vishnu Jaiswal <30700361+zAcKeR-KrAcKeR@users.noreply.github.com>
2025-07-29 01:00:41 +0530

    Update supabase_connect.py

--- database/supabase_connect.py
+++ database/supabase_connect.py
@@ -2,9 +2,11 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DB_URL = os.getenv("SUPABASE_DB_URL")
+DB_URL = os.getenv("SUPABASE_DB")
+if DB_URL is None:
+    DB_URL = os.getenv("SUPABASE_DB_URL")
 if not DB_URL:
-    raise ValueError("SUPABASE_DB_URL not set in env.")
+    raise ValueError("SUPABASE_DB or SUPABASE_DB_URL environment variable is not set.")
 
 engine = create_engine(DB_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
