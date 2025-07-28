209880f502c9bf2b270994312c99e3245419007c
Vishnu Jaiswal <30700361+zAcKeR-KrAcKeR@users.noreply.github.com>
2025-07-28 17:47:28 +0530

    Update blob_storage.py

--- utils/blob_storage.py
+++ utils/blob_storage.py
@@ -4,7 +4,6 @@ from azure.storage.blob import BlobServiceClient
 from dotenv import load_dotenv
 
 load_dotenv()
-
 logger = logging.getLogger(__name__)
 
 class AzureBlobStorage:
@@ -14,19 +13,17 @@ class AzureBlobStorage:
         self.container = os.getenv("AZURE_CONTAINER_NAME", "audio-files")
 
         if not self.account_name or not self.account_key:
-            raise ValueError("AZURE_STORAGE_ACCOUNT_NAME or AZURE_STORAGE_ACCOUNT_KEY not set in environment variables")
+            raise ValueError("Azure Storage account name/key missing in environment variables")
 
         connection_str = (
             f"DefaultEndpointsProtocol=https;AccountName={self.account_name};"
             f"AccountKey={self.account_key};EndpointSuffix=core.windows.net"
         )
-
         self.client = BlobServiceClient.from_connection_string(connection_str)
 
     def upload_audio_file(self, file_path, blob_name=None):
         if not blob_name:
             blob_name = os.path.basename(file_path)
-
         try:
             blob_client = self.client.get_blob_client(self.container, blob_name)
             with open(file_path, "rb") as data:
@@ -39,4 +36,3 @@ class AzureBlobStorage:
             return None
 
 blob_storage = AzureBlobStorage()
-
